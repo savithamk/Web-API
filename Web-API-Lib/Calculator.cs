43260f5fa2b4ec4f1b85ad9cd5b0d9ccf14f02d6
savithamk <savitha.maiya1@gmail.com>
2022-07-01 16:28:28 -0400

    Created a ASP.Net project with Web API, Test project and ASP.Net core Library. Added Addition, Subtraction, Multiplication and division logic in core library.

---
+++ Web-API-Lib/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Web_API_Lib
+{
+    public class Calculator
+    {
+        double Add(double left, double right)
+        {
+            return left + right;
+        }
+
+        double Subtraction(double left, double right)
+        {
+            return left - right;
+        }
+
+        double Multiplication(double left, double right)
+        {
+            return left * right;
+        }
+
+        double Division(double left, double right)
+        {
+            return left / right;
+        }
+    }
+}
---
+++ Web-API-Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Web_API_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
