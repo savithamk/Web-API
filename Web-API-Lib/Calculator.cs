5d21d90a1f0bf33c59dae710f7eace0dd5568d00
savithamk <savitha.maiya1@gmail.com>
2022-07-14 22:32:50 -0400

    Added comments in each method

--- Web-API-Lib/Calculator.cs
+++ Web-API-Lib/Calculator.cs
@@ -4,21 +4,25 @@ namespace Web_API_Lib
 {
     public class Calculator
     {
+        //To add two numbers
         public static double Add(double left, double right)
         {
             return left + right;
         }
 
+        //To subtract two numbers
         public static double Subtract(double left, double right)
         {
             return left - right;
         }
 
+        //To multiply two numbers
         public static double Multiply(double left, double right)
         {
             return left * right;
         }
 
+        //To divide two numbers
         public static double Divide(double left, double right)
         {
             if(right != 0)
--- Web-API/Controllers/CalculatorController.cs
+++ Web-API/Controllers/CalculatorController.cs
@@ -18,24 +18,29 @@ namespace Web_API.Controllers
             _logger = logger;
         }
 
+
+        //GET http method to add two numbers
         [HttpGet("Addition")]
         public double Addition(double left, double right)
         {
             return Web_API_Lib.Calculator.Add(left, right);
         }
 
+        //GET http method to subtract two numbers
         [HttpGet("Subtraction")]
         public double Subtraction(double left, double right)
         {
             return Web_API_Lib.Calculator.Subtract(left, right);
         }
 
+        //GET http method to multiply two numbers
         [HttpGet("Multiplication")]
         public double Multiplication(double left, double right)
         {
             return Web_API_Lib.Calculator.Multiply(left, right);
         }
 
+        //GET http method to divide two numbers
         [HttpGet("Division")]
         public double Division(double left, double right)
         {
